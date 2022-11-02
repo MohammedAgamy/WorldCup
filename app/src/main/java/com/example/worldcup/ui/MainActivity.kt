d8da9814687610626d550d8d854abadc88ee4563
MohamedAgamy <kemetapp207@gmail.com>
2022-11-02 22:09:07 +0200

    addFile

--- app/src/main/java/com/example/worldcup/ui/MainActivity.kt
+++ app/src/main/java/com/example/worldcup/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.worldcup.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.worldcup.R
 import com.example.worldcup.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.Buffer
 
 class MainActivity : AppCompatActivity() {
     lateinit var  binding:ActivityMainBinding
@@ -11,5 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        openFile()
+    }
+
+
+    fun openFile()
+    {
+        val inPutStream=assets.open("worldcup.csv")
+        val buffer=BufferedReader(InputStreamReader(inPutStream))
+
+        buffer.forEachLine { Log.e("main" , it) }
     }
 }
